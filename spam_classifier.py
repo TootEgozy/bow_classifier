56df43b6800c0448e75bb2974d3aa409e336a91c
TootEgozy <tootegozy@gmail.com>
2024-01-31 22:10:33 +0200

    cleanup before adding tests

--- spam_classifier.py
+++ spam_classifier.py
@@ -1,19 +1,3 @@
-# process explained:
-# we get training data, convert it into a bow and then to a feature dictionary.
-# each unique word in the dictionary is a column for the matrix.
-# we decide on how to divide our data into documents. a document can be a word, a sentence, a row etc.
-# each row in our matrix is the vectorised version of the document. 0's where words are not present and
-# their count where they are.
-# separate from that, we have our label's list. it's the length of our documents list length.
-# now we have our data ready.
-# we get a new input, and convert it to a vector: convert it into a BoW model, create an empty vector,
-# and fill out the word count for each place in the vector (if the word is in our dictionary).
-# MultinomialNB scans patterns in our matrix and finds the row which is most approximate to our vector.
-# it returns the label from the labels list which matches the row in our matrix,
-# and that's how our data gets classified.
-# MultinomialNB can also give us it's accuracy estimation.
-
-# convert the spam & ham file into a dictionary
 import csv
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
@@ -34,17 +18,14 @@ def process_learning_data(filepath):
     X_bow = vectorizer.fit_transform(texts)
     return X_bow, labels, vectorizer
 
-# TODO: write the vectirization function for the new input
 def input_to_vector(input, vectorizer):
     str_vec = vectorizer.transform([input])
     # print(str_vec.nonzero())
     return str_vec
 
 [X_bow, labels, vectorizer] = process_learning_data('spam.csv')
-# print(X_bow.shape)
 input_vec = input_to_vector(input, vectorizer)
 
-# add MultinomialNB and test a few inputs
 X_train, X_test, y_train, y_test = train_test_split(X_bow, labels, test_size=0.2, random_state=42)
 
 spam_classifier = MultinomialNB()
