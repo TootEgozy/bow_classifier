1b161f26451b9603de109dcdb4321f8644e6ed97
TootEgozy <tootegozy@gmail.com>
2024-01-31 11:33:43 +0200

    basic spam classifier

--- spam_classifier.py
+++ spam_classifier.py
@@ -16,9 +16,12 @@
 # convert the spam & ham file into a dictionary
 import csv
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
 
 
-def create_dictionary(filepath):
+input = 'hello'
+def process_learning_data(filepath):
     texts = list()
     labels = list()
     with open(filepath, 'r', encoding='latin1') as file:
@@ -29,14 +32,32 @@ def create_dictionary(filepath):
             texts.append(row[1].strip('"'))
     vectorizer = CountVectorizer()
     X_bow = vectorizer.fit_transform(texts)
-    print(X_bow.shape)
-
-create_dictionary('spam.csv')
+    return X_bow, labels, vectorizer
 
 # TODO: write the vectirization function for the new input
+def input_to_vector(input, vectorizer):
+    str_vec = vectorizer.transform([input])
+    # print(str_vec.nonzero())
+    return str_vec
+
+[X_bow, labels, vectorizer] = process_learning_data('spam.csv')
+# print(X_bow.shape)
+input_vec = input_to_vector(input, vectorizer)
+
 # add MultinomialNB and test a few inputs
+X_train, X_test, y_train, y_test = train_test_split(X_bow, labels, test_size=0.2, random_state=42)
+
+spam_classifier = MultinomialNB()
+spam_classifier.fit(X_train, y_train)
+
+predictions = spam_classifier.score(X_test, y_test)
+predicted_label = spam_classifier.predict(input_vec)
+
+print("Predicted Label:", predicted_label[0])
+print("Accuracy:", predictions * 100)
 # learn how to add tests in python and write some tests
 
-# TODO: poish the work and finish the flow
+
+# TODO: polish the work and finish the flow
 # connect frontend
 # go live
