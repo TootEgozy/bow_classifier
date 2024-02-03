abf3d1b95c8953baa3d05b36df75b810238d713e
TootEgozy <tootegozy@gmail.com>
2024-02-03 10:27:48 +0200

    tests

--- test_spam_classifier.py
+++ test_spam_classifier.py
@@ -1,6 +1,19 @@
 from spam_classifier import classify_spam
 
-def test_classify_spam():
-    predicted_label, predictions_accuracy = classify_spam('FREE')
-    assert predicted_label == 'spam'
-    assert predictions_accuracy > 90
+# TODO: add a better testing flow
+# write a general function to sanitize data from files to provide the best learning data
+# test individual functions and the flow using the sanitized data, try to get the prediction's accuracy in a range
+
+class TestClassfier:
+
+    def test_classify_spam(self):
+        predicted_label, predictions_accuracy = classify_spam('FREE')
+        print(predicted_label, predictions_accuracy)
+        assert predicted_label == 'spam'
+        assert predictions_accuracy > 90
+
+    def test_classify_not_spam(self):
+        predicted_label, predictions_accuracy = classify_spam('hi')
+        print(predicted_label, predictions_accuracy)
+        assert predicted_label == 'ham'
+        assert predictions_accuracy > 90
