d25d088a06a837e20ac4e0671cde8e07939838b4
TootEgozy <tootegozy@gmail.com>
2024-02-23 10:21:37 +0200

    unit tests (in progress)

--- tests/test_spam_classifier.py
+++ tests/test_spam_classifier.py
@@ -1,19 +1,26 @@
-from index import classify_input
+import pytest
+import asyncio
 
-# TODO: add a better testing flow
-# write a general function to sanitize data from files to provide the best learning data
-# test individual functions and the flow using the sanitized data, try to get the prediction's accuracy in a range
+from index import classify_input, process_learning_data
 
-class TestClassfier:
+from utils.get_text_by_label import get_texts_by_label
 
-    def test_classify_spam(self):
-        predicted_label, predictions_accuracy = classify_input('FREE')
-        print(predicted_label, predictions_accuracy)
-        assert predicted_label == 'spam'
-        assert predictions_accuracy > 90
 
-    def test_classify_not_spam(self):
-        predicted_label, predictions_accuracy = classify_input('hi')
-        print(predicted_label, predictions_accuracy)
-        assert predicted_label == 'ham'
-        assert predictions_accuracy > 90
+@pytest.fixture(scope="session")
+async def setup_data():
+    return await process_learning_data()
+
+class TestClassifier:
+
+    def test_classify_spam(setup_data):
+        spam_texts = get_texts_by_label('spam', 5)
+
+        for text in spam_texts:
+            predicted_label = classify_input(text, 'spam', setup_data)
+            print(predicted_label)
+            assert predicted_label == 'spam'
+
+    # def test_classify_ham(self):
+    #     predicted_label, predictions_accuracy = classify_input('hi')
+    #     print(predicted_label, predictions_accuracy)
+    #     assert predicted_label == 'ham'
--- utils/get_text_by_label.py
+++ utils/get_text_by_label.py
@@ -2,10 +2,23 @@ import os
 import random
 import pandas as pd
 
-def get_texts_by_label(label, count, cls_type):
-    texts = set()
-    filename = random.choice(os.listdir(f'learning_data/{cls_type}'))
-    df = pd.read_csv(f'learning_data/{cls_type}/{filename}', encoding='latin1')
+def get_data_path(label):
+    # Get the absolute path of the current script file
+    current_script_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+    # Navigate up to the root directory of the project
+    root_dir = os.path.dirname(current_script_dir)
+
+    # Construct the path to the learning_data folder
+    learning_data_path = os.path.join(root_dir, 'learning_data', label)
+    return learning_data_path
+
+
+def get_texts_by_label(label, count):
+    dpath = get_data_path(label)
+    filename = random.choice(os.listdir(dpath))
+    df = pd.read_csv(f'{dpath}/{filename}', encoding='latin1')
     filtered_df = df[df.iloc[:, 0] == label]
     random_samples = filtered_df.sample(n=count, random_state=None).iloc[:, 1].dropna().tolist()
     return random_samples
