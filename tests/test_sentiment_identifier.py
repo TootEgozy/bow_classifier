40a33b6090382d62f3dd7bd9bcc7d8c4f699b5f2
TootEgozy <tootegozy@gmail.com>
2024-02-27 15:31:58 +0200

    tests

--- tests/test_sentiment_identifier.py
+++ tests/test_sentiment_identifier.py
@@ -9,18 +9,18 @@ from utils.get_text_by_label import get_texts_by_label
 def learning_data():
     return process_learning_data()
 
-
+# These tests are unstable. some labels in the csv files are wrong (good training data is hard to come by)
+# and the model classifies the texts correctly, so the test fails
 class TestSentimentClassifier:
 
     def test_classify_negative(self, learning_data):
-        negative_inputs = get_texts_by_label('sentiment', '0', 5, 5,'sentiment_1.csv')
-        print(negative_inputs)
+        negative_inputs = get_texts_by_label('sentiment', '0', 5, 5,'sentiment_1.csv') + get_texts_by_label('sentiment', '0', 5, 5,'sentiment_2.csv')
         for input in negative_inputs:
             predicted_label = classify_input(input, 'sentiment', learning_data['sentiment'])
-            assert predicted_label == 'negative'
+            assert predicted_label == 'negative', f"Assertion failed for input = {input}"
 
-    def test_classify_ham(self, learning_data):
-        ham_inputs = get_texts_by_label('spam','ham', 10)
-        for input in ham_inputs:
-            predicted_label = classify_input(input, 'ham', learning_data['spam'])
-            assert predicted_label == 'ham'
+    def test_classify_positive(self, learning_data):
+        positive_inputs = get_texts_by_label('sentiment', '4', 5, 5,'sentiment_4.csv') + get_texts_by_label('sentiment', '4', 5, 5,'sentiment_5.csv')
+        for input in positive_inputs:
+            predicted_label = classify_input(input, 'sentiment', learning_data['sentiment'])
+            assert predicted_label == 'positive', f"Assertion failed for input = {input}"
--- utils/get_text_by_label.py
+++ utils/get_text_by_label.py
@@ -9,8 +9,6 @@ def get_texts_by_label(cls_type, label, count, text_column_index, set_filename=N
     dpath = get_data_path(cls_type)
     filename = set_filename if set_filename else random.choice(os.listdir(dpath))
     df = pd.read_csv(f'{dpath}/{filename}', encoding='latin1')
-    print(' ')
-    print(df.head())
     filtered_df = df[df.iloc[:, 0].astype(str) == str(label)]
     random_samples = filtered_df.sample(n=count, random_state=None).iloc[:, text_column_index].dropna().tolist()
     return random_samples
