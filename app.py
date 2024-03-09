c9401ab81e4d537cac7dcbbcfe6910af832c8398
TootEgozy <tootegozy@gmail.com>
2024-03-09 22:42:10 +0200

    generating inputs for user wip (failing)

--- app.py
+++ app.py
@@ -20,15 +20,19 @@ def index():
 
 @app.route('/generate-inputs', methods=['POST'])
 def generate_inputs():
-    body = request.data.decode('utf-8')
-    body_dict = json.loads(body)
-    print('got a request from client')
-    print(body)
-    cls_type = body.get('cls_type')
-    count = body.get('count')
-    inputs = get_inputs_for_user('spam', 5)
+    # body = request.data.decode('utf-8')
+    # body_dict = json.loads(body)
+    # print('got a request from client')
+    # print(body)
+    # cls_type = body.get('cls_type')
+    # count = body.get('count')
+    # inputs = get_inputs_for_user('spam', 5)
     # return jsonify({'inputs': inputs})
     # new_inputs = ['Input 1', 'Input 2', 'Input 3']
+    data = request.get_json()  # Get JSON data from the request
+    cls_type = data.get('cls_type')
+    count = data.get('count')
+    inputs = get_inputs_for_user(cls_type, count)  # Use cls_type and count from the request
     return jsonify(inputs=inputs)
 
 
@@ -42,6 +46,6 @@ def classifier():
 
 if __name__ == '__main__':
     # dev server
-    # app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
     # production server
-    serve(app, host='0.0.0.0', port=5000)
+    # serve(app, host='0.0.0.0', port=5000)
