d757b048b3b9c124c6dc7a6abe0d37b78f883a8f
TootEgozy <tootegozy@gmail.com>
2024-03-30 22:31:52 +0300

    a loading page which redirects the user to the main page after the learning data processing is done

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
 from flask import Flask, render_template, request, jsonify
 from index import process_learning_data, classify_input, get_inputs_for_user
-import json
-from waitress import serve
 
 app = Flask(__name__)
 
 learning_data = {}
 
-@app.before_request
-def initialize():
-    global learning_data
-    if not learning_data:
-        learning_data = process_learning_data()
-        print('added learning data')
-
+#TODO: write it more clean or split into 2 routes
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        input_text = request.form.get('input_text')
-        input_type = request.form.get('input_type')
-        cls_result = classify_input(input_text, input_type, learning_data[input_type])
-        return render_template('index.html', classification=cls_result)
+    if learning_data:
+        if request.method == 'POST':
+            input_text = request.form.get('input_text')
+            input_type = request.form.get('input_type')
+            cls_result = classify_input(input_text, input_type, learning_data[input_type])
+            return render_template('index.html', classification=cls_result)
+        else:
+            return render_template('index.html', classification=None)
     else:
-        return render_template('index.html', classification=None)
+        return render_template('loading.html')
 
 @app.route('/generate-inputs', methods=['POST'])
 def generate_inputs():
@@ -34,6 +29,15 @@ def generate_inputs():
         return jsonify(inputs=inputs)
     except Exception as e:
         return jsonify(error=str(e)), 400
+
+@app.route('/process_learning_data', methods=['GET'])
+def initialize_learning_data():
+    global learning_data
+    if not learning_data:
+        learning_data = process_learning_data()
+        print('added learning data')
+        return jsonify({'success': True})
+
 
 if __name__ == '__main__':
     # dev server
