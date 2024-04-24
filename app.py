90f1c279e4202fd5b5dc7f6afefbb2762c91cc5f
TootEgozy <tootegozy@gmail.com>
2024-04-24 07:22:49 +0300

    rest api - sending a request and getting an answer once processing is done

--- app.py
+++ app.py
@@ -8,30 +8,30 @@ CORS(app)
 
 learning_data = {}
 
-#TODO: write it more clean or split into 2 routes
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if learning_data:
-        if request.method == 'POST':
-            input_text = request.form.get('input_text')
-            input_type = request.form.get('input_type')
-            cls_result = classify_input(input_text, input_type, learning_data[input_type])
-            return render_template('index.html', classification=cls_result)
-        else:
-            return render_template('index.html', classification=None)
-    else:
-        return render_template('loading.html')
-
-@app.route('/generate-inputs', methods=['POST'])
-def generate_inputs():
-    try:
-        data = request.get_json()
-        cls_type = data.get('cls_type')
-        count = data.get('count')
-        inputs = get_inputs_for_user(cls_type, count)
-        return jsonify(inputs=inputs)
-    except Exception as e:
-        return jsonify(error=str(e)), 400
+# #TODO: write it more clean or split into 2 routes
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if learning_data:
+#         if request.method == 'POST':
+#             input_text = request.form.get('input_text')
+#             input_type = request.form.get('input_type')
+#             cls_result = classify_input(input_text, input_type, learning_data[input_type])
+#             return render_template('index.html', classification=cls_result)
+#         else:
+#             return render_template('index.html', classification=None)
+#     else:
+#         return render_template('loading.html')
+#
+# @app.route('/generate-inputs', methods=['POST'])
+# def generate_inputs():
+#     try:
+#         data = request.get_json()
+#         cls_type = data.get('cls_type')
+#         count = data.get('count')
+#         inputs = get_inputs_for_user(cls_type, count)
+#         return jsonify(inputs=inputs)
+#     except Exception as e:
+#         return jsonify(error=str(e)), 400
 
 @app.route('/process_learning_data', methods=['GET'])
 def initialize_learning_data():
@@ -39,15 +39,10 @@ def initialize_learning_data():
     if not learning_data:
         learning_data = process_learning_data()
         print('added learning data')
-        return jsonify({'success': True})
+        return jsonify({'processing_done': True})
+    else:
+        return jsonify({'processing_done': True})
 
-
-@app.route('/events')
-def events():
-    # Simulate data processing (replace with your actual logic)
-    import time
-    time.sleep(10)  # Simulate 5 seconds of processing
-    return sse.publish({'message': 'Server is ready!'})
 
 if __name__ == '__main__':
     # dev server
