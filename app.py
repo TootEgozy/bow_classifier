a22c7011477e119fcc2558686046d8744c383da4
TootEgozy <tootegozy@gmail.com>
2024-05-14 23:24:05 +0300

    rest api

--- app.py
+++ app.py
@@ -1,38 +1,14 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from index import process_learning_data, classify_input, get_inputs_for_user
 from flask_cors import CORS
-from flask_sse import sse
 
 app = Flask(__name__)
 CORS(app)
 
 learning_data = {}
 
-# #TODO: write it more clean or split into 2 routes
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if learning_data:
-#         if request.method == 'POST':
-#             input_text = request.form.get('input_text')
-#             input_type = request.form.get('input_type')
-#             cls_result = classify_input(input_text, input_type, learning_data[input_type])
-#             return render_template('index.html', classification=cls_result)
-#         else:
-#             return render_template('index.html', classification=None)
-#     else:
-#         return render_template('loading.html')
-#
-# @app.route('/generate-inputs', methods=['POST'])
-def generate_inputs():
-     # try:
-         data = request.get_json()
-         cls_type = data.get('cls_type')
-         count = data.get('count')
-         inputs = get_inputs_for_user(cls_type, count)
-         return jsonify(inputs=inputs)
-     # except Exception as e:
-         # return jsonify(error=str(e)), 400
 
+# make sure that learning data is loaded
 @app.route('/process_learning_data', methods=['GET'])
 def initialize_learning_data():
     global learning_data
@@ -40,12 +16,38 @@ def initialize_learning_data():
         learning_data = process_learning_data()
         print('added learning data')
         return jsonify({'processing_done': True})
-     else:
-         return jsonify({'processing_done': True})
+    else:
+        return jsonify({'processing_done': True})
+
+# get the cls type and a count from the user and return sample inputs
+
+
+@app.route('/generate-inputs', methods=['POST'])
+def generate_inputs():
+    try:
+        data = request.get_json()
+        cls_type = data.get('cls_type')
+        count = data.get('count')
+        inputs = get_inputs_for_user(cls_type, count)
+        return jsonify(inputs=inputs)
+    except Exception as e:
+        return jsonify(error=str(e)), 400
+
+
+# classify input
+@app.route('/classify', methods=['POST'])
+def index():
+    if learning_data:
+        input_text = request.form.get('input_text')
+        cls_type = request.form.get('cls_type')
+        cls_result = classify_input(input_text, cls_type, learning_data[cls_type])
+        return jsonify(result=cls_result)
+    else:
+        return jsonify({'error': 'missing learning data'})
 
 
 if __name__ == '__main__':
     # dev server
-    # app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
     # production server
-    serve(app, host='0.0.0.0', port=5000)
+    # serve(app, host='0.0.0.0', port=5000)
