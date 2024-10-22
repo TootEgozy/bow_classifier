74b22c9a02e0f91d62c65b47536939071347250e
TootEgozy <tootegozy@gmail.com>
2024-10-22 15:43:43 +0300

    fixed windows based separators to support unix

--- utils/get_data_path.py
+++ utils/get_data_path.py
@@ -3,6 +3,5 @@ import os
 
 def get_data_path(cls_type):
     root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    learning_data_path = os.path.join(root_dir, f'learning_data\\{cls_type}')
-
+    learning_data_path = os.path.join(root_dir, 'learning_data', cls_type)
     return learning_data_path
