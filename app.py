4e2b8f685aae1a1a77b3047704a1ade8f7ab4c6b
TootEgozy <tootegozy@gmail.com>
2025-03-09 19:50:09 +0200

    unified responses, error catching and handling, inclusive middleware, avoiding restart of the learning_data thread if it's already in process

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ CORS(app)
 
 learning_data = None
 learning_data_lock = threading.Lock()
+data_loading_thread = None
 
 port = int(os.environ.get("PORT", 5000))
 request_limiter = RequestLimiter()
@@ -36,54 +37,62 @@ def unload_learning_data():
 app.config['load_learning_data'] = load_learning_data
 app.config['unload_learning_data'] = unload_learning_data
 
+# middleware to check if requests are blocked, resetting the memory_unloader timer,
+# and if learning _data is missing load it
 @app.before_request
 def before_request():
+    global learning_data, data_loading_thread
     blocked = request_limiter.check_handle_limit_reached()
     if blocked:
-        return make_response(jsonify('requests are blocked'), 429)
+        return make_response(jsonify({"message": "Requests are blocked, please try again later"}), 429)
     else:
-        if learning_data is not None:
-            memory_unloader.reset_timer()
-        else:
-            if request.path != "/server_ready":
-                threading.Thread(target=load_learning_data).start()
-                memory_unloader.reset_timer()
-                return make_response(jsonify('missing learning data'), 503)
+        memory_unloader.reset_timer()
+        if learning_data is None:
+            if data_loading_thread is None or not data_loading_thread.is_alive():
+                data_loading_thread = threading.Thread(target=load_learning_data)
+                data_loading_thread.start()
+            return make_response(jsonify({"message": "Missing learning data"}), 503)
 
 
-# make sure that learning data is loaded
-# TODO: use a websocket for this
+# an endpoint to test if the server is ready, if we reached it then learning data is loaded.
 @app.route('/server_ready', methods=['GET'])
 def initialize_learning_data():
-   global learning_data
-   if not learning_data:
-       load_learning_data() # wait for this process
-       return jsonify({'server_ready': True}, 200)
-   else:
-        return jsonify({'server_ready': True}, 200)
+    return make_response("", 204)
 
-# get the cls type and a count from the user and return sample inputs
+# get the classification type and a count from the user and return sample inputs
 @app.route('/generate_inputs', methods=['POST'])
 def generate_inputs():
     try:
         data = request.get_json()
         cls_type = data.get('cls_type')
         count = data.get('count')
+
+        if cls_type is None or count is None:
+            return make_response(jsonify({"error": "Missing 'cls_type' or 'count'"}), 400)
+
         inputs = get_inputs_for_user(cls_type, count)
-        return jsonify(inputs=inputs)
+        return make_response(jsonify({"inputs": inputs}), 200)
     except Exception as e:
-        return jsonify(error=str(e)), 400
+        print(e)
+        return make_response(jsonify({"error": "Internal server error"}), 500)
 
 
-# classify input
+# classify input from user
 @app.route('/classify', methods=['POST'])
 def index():
-    data = request.get_json()
-    input_text = data.get('input_text')
-    cls_type = data.get('cls_type')
-    cls_result = classify_input(input_text, cls_type, learning_data[cls_type])
-    return jsonify(result=cls_result)
+    try:
+        data = request.get_json()
+        input_text = data.get('input_text')
+        cls_type = data.get('cls_type')
 
+        if cls_type is None or input_text is None:
+            return make_response(jsonify({"error": "Missing 'cls_type' or 'input_text'"}), 400)
+
+        cls_result = classify_input(input_text, cls_type, learning_data[cls_type])
+        return make_response(jsonify({"result": cls_result}), 200)
+    except Exception as e:
+        print(e)
+        return make_response(jsonify({"error": "Internal server error"}), 500)
 
 
 if __name__ == '__main__':
