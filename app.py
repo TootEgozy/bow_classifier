9e13d2629dd6975094dc62186636d847e92aa2e2
TootEgozy <tootegozy@gmail.com>
2025-03-16 21:42:30 +0200

    wip - splitting learning data to spam or sentiment

--- app.py
+++ app.py
@@ -56,11 +56,9 @@ def before_request():
         memory_unloader.reset_timer()
         request_cls_type = request.args.get("cls_type") or "spam"
         if learning_data is None or request_cls_type != cls_type:
-            # TODO: continue checking the conditions here to start a new thread
-            if data_loading_thread is None or not data_loading_thread.is_alive():
-                cls_type = request.args.get("cls_type") or "spam"
-                data_loading_thread = threading.Thread(target=load_learning_data, args=(cls_type,))
-                data_loading_thread.start()
+            cls_type = request.args.get("cls_type") or "spam"
+            data_loading_thread = threading.Thread(target=load_learning_data, args=(cls_type,))
+            data_loading_thread.start()
             return make_response(jsonify({"message": "Missing learning data"}), 503)
 
 
