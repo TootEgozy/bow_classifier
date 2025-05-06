769382e3313ede783662277bfbff2e78b8633486
TootEgozy <tootegozy@gmail.com>
2025-05-06 16:26:43 +0300

    wip joining learning data objects

--- app.py
+++ app.py
@@ -16,16 +16,16 @@ CORS(app)
 # learning_data is the object where the vector matrix is saved.
 learning_data = None
 learning_data_lock = threading.Lock()
+data_loading_in_progress = False
 port = int(os.environ.get("PORT", 5000))
 request_limiter = RequestLimiter()
 
 
 def load_learning_data():
-    global learning_data, learning_data_lock
-    with learning_data_lock:
-        learning_data = process_learning_data()
-        print('added learning data')
-        log_memory()
+    global learning_data
+    learning_data = process_learning_data()
+    print('added learning data')
+    log_memory()
 
 
 def unload_learning_data():
@@ -33,6 +33,22 @@ def unload_learning_data():
     with learning_data_lock:
         learning_data = None
         print("unloaded learning data")
+
+def start_learning_data_load_thread():
+    global data_loading_in_progress
+
+    with learning_data_lock:
+        if not data_loading_in_progress:
+            data_loading_in_progress = True
+
+            def load_in_background():
+                global data_loading_in_progress
+                try:
+                    load_learning_data()
+                finally:
+                    data_loading_in_progress = False
+
+            threading.Thread(target=load_in_background, daemon=True).start()
 
 
 # append these methods to app, so that dependencies can access them.
@@ -50,7 +66,7 @@ def before_request():
             return make_response(jsonify({"message": "Requests are blocked, please try again later"}), 429)
         else:
             if learning_data is None:
-                load_learning_data()
+                start_learning_data_load_thread()
                 return make_response(jsonify({"message": "Missing learning data"}), 503)
     finally:
         log_memory()
@@ -99,10 +115,11 @@ def index():
         print(e)
         return make_response(jsonify({"error": "Internal server error"}), 500)
 
+# load learning data before starting app
+start_learning_data_load_thread()
 
 if __name__ == '__main__':
-    # dev server
-    # app.run(host='0.0.0.0', port=5000)
+    # dev server - use:
+    # gunicorn -w 4 app:app
     # production server
     serve(app, host='0.0.0.0', port=port)
-    load_learning_data()
